e7ed12fb5d8bf58ccb57b67eb30cd34594210e91
Nooruldeen82 <nooraldeenali82@gmail.com>
2019-11-23 20:38:20 +0100

    Updates

---
+++ precache-manifest.f960d0b68d35564ec99395ac8c1b5eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb7a6bd32133dda71078",
+    "url": "/galleryapp/static/css/main.27336299.chunk.css"
+  },
+  {
+    "revision": "bb7a6bd32133dda71078",
+    "url": "/galleryapp/static/js/main.bb7a6bd3.chunk.js"
+  },
+  {
+    "revision": "619bdb85ce4929cc3116",
+    "url": "/galleryapp/static/js/1.619bdb85.chunk.js"
+  },
+  {
+    "revision": "fdef1e4cf7d6d6471312",
+    "url": "/galleryapp/static/js/runtime~main.fdef1e4c.js"
+  },
+  {
+    "revision": "1175b2ee024aa63b5de151285c8fb988",
+    "url": "/galleryapp/index.html"
+  }
+];
